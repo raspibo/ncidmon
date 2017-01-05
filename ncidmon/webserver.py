c204925a1769c33eb482a2064b4fe06262a166cc
daniele <dcast78@gmail.com>
2017-01-05 21:25:07 +0100

    Black and white list created

--- ncidmon/webserver.py
+++ ncidmon/webserver.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 # apt-get twisted-twisted  or  apt-get python-twisted-core
 from twisted.web import resource
@@ -8,6 +9,8 @@ from twisted.web import resource
 from .cidentry import CIDEntry
 from .misc import CONFIG
 
+import os
+from urlparse import urlparse
 
 class CallListServer(resource.Resource):
     
@@ -19,6 +22,7 @@ class CallListServer(resource.Resource):
         self.update_call_list()
     
     
+
     def update_call_list(self, call_list=None):
         '''update rendered HTML list'''
         
@@ -38,10 +42,12 @@ class CallListServer(resource.Resource):
                     caller = entry.resolve_number()
                     if caller is None:
                         # the caller is unknown: build lookup links
-                        caller = '&#10140;&nbsp;' + ', '.join(
+                        caller = 'Check caller reputation: &#10140;&nbsp;' + ', '.join(
                             '<a href="{0}" target="_blank">{1}</a>'.format(
                                 url.format(number=number), name)
                             for name, url in CONFIG['NUMBER_LOOKUP_PAGES'])
+			blacklistlink = 'Add to blacklist:&nbsp;<a href="/?blacklist=' + pretty_number +'">' + pretty_number + '</a>'
+			whitelistlink = 'Add to whitelist:&nbsp;<a href="/?whitelist=' + pretty_number +'">' + pretty_number + '</a>'
                 else:
                     # no telephone number, unknown caller, lookup not possible
                     tel = pretty_number  # 'Anonymous' or similiar
@@ -52,15 +58,48 @@ class CallListServer(resource.Resource):
                         {0}
                         <p><name>{1}</name></p>
                         <p><date>{2}</date> <time>{3}</time></p>
+                        <p><name>{4}</name></p>
+                        <p><name>{5}</name></p>
                     </li>
                     '''.format(
                     tel, caller, entry.get_pretty_date(),
-                    entry.get_pretty_time())
+                    entry.get_pretty_time(), blacklistlink, whitelistlink)
             
             self._rendered_call_list += '</ol>'
-    
+ 
     
     def render_GET(self, request):
+	if request.method == 'GET':     
+	    if request.args.get('blacklist') is not None:
+	        number = request.args.get('blacklist')
+                print "Adding number to blacklist" + str(number)
+	        try:
+	            fp = open("/usr/ncidmon/ncidmon/var/lists/it/ncidmon.blacklist",'a')
+    	        except IOError as e:
+                    if e.errno :
+                        print "File access error"
+                    raise
+	        else:
+                    with fp:
+                        print number[0]
+                        fp.write(number[0]) 
+                        fp.write("\n")
+		        fp.close() 
+	    if request.args.get('whitelist') is not None:
+	        number = request.args.get('whitelist')
+                print "Adding number to whitelist" + str(number)
+	        try:
+	            fp = open("/usr/ncidmon/ncidmon/var/lists/it/ncidmon.whitelist",'a')
+    	        except IOError as e:
+                    if e.errno :
+                        print "File access error"
+                    raise
+	        else:
+                    with fp:
+                        print number[0]
+                        fp.write(number[0]) 
+                        fp.write("\n")
+		        fp.close()
         return PAGE_HEADER + self._rendered_call_list + PAGE_FOOTER
 
 
